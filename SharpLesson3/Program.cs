86866978f09a7c893bd00c0e6e4a37829923fa6e
1PALADIN1 <sonikx94@yandex.ru>
2018-10-14 13:30:22 +0300

    Задание 1 (reverted from commit 4565b506a436edaf4cf8664d3414e1887e347501)

--- SharpLesson3/Program.cs
+++ SharpLesson3/Program.cs
@@ -1,48 +1,13 @@
-﻿/* Малиновский Руслан */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SharpLesson3 {
-    partial class Program {
+    class Program {
         static void Main(string[] args) {
-            Menu();
 
-            FinishProgram();
-        }
-
-        static void Menu() {
-            Console.WriteLine("Задания:\n0. Выход\n1. Структура Complex");
-            bool work = true;
-
-            do {
-                Console.Write("\nВведите номер задания: ");
-                var key = Console.ReadKey();
-                Console.WriteLine();
-
-                switch (key.Key) {
-                    case ConsoleKey.D0:
-                    case ConsoleKey.NumPad0:
-                        work = false;
-                        break;
-                    case ConsoleKey.D1:
-                    case ConsoleKey.NumPad1:
-                        Task1();
-                        break;
-                    default:
-                        Console.WriteLine("Команда не распознана!");
-                        break;
-                }
-            } while (work);
-            
-        }
-
-        static void FinishProgram() {
-            Console.WriteLine("Программа завершила свою работу, нажмите любую клавишу для выхода");
-            Console.ReadKey();
         }
     }
 }
