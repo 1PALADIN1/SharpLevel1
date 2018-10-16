d439d27680bbdb02fe21966dbc297f708449dc42
1PALADIN1 <sonikx94@yandex.ru>
2018-10-16 20:38:54 +0300

    init

---
+++ SharpLesson4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLesson4 {
+    partial class Program {
+        static void Main(string[] args) {
+        }
+
+        static void Menu() {
+            Console.WriteLine("Задания:\n0. Выход\n1. Структура Complex\n2. Сумма положительных четных чисел\n3. Дробные числа");
+            bool work = true;
+
+            do {
+                Console.Write("\nВведите номер задания: ");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key) {
+                    case ConsoleKey.D0:
+                    case ConsoleKey.NumPad0:
+                        work = false;
+                        break;
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        //Task1();
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        //Task2();
+                        break;
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        //Task3();
+                        break;
+                    default:
+                        Console.WriteLine("Команда не распознана!");
+                        break;
+                }
+            } while (work);
+        }
+
+        static void FinishProgram() {
+            Console.WriteLine("Программа завершила свою работу, нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
