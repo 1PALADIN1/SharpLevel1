92b3777ff68c58e106f569f96f90581d0ed67853
1PALADIN1 <sonikx94@yandex.ru>
2018-10-27 21:29:52 +0300

    Задание 1. Добавил класс "Удвоитель"

---
+++ SharpLesson7Doubler/Doubler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//класс "удвоитель"
+namespace SharpLesson7Doubler {
+    class Doubler {
+        int finishNumber;
+        int currentNumber;
+
+        public int FinishNumber {
+            get {
+                return finishNumber;
+            }
+        }
+
+        public int CurrentNumber {
+            get {
+                return currentNumber;
+            }
+        }
+
+        public Doubler(int min, int max) {
+            Random rand = new Random();
+            finishNumber = rand.Next(min, max + 1);
+            currentNumber = 1;
+        }
+
+        public void Plus() {
+            currentNumber++;
+        }
+
+        public void Multiply() {
+            currentNumber *= 2;
+        }
+
+        public void Reset() {
+            currentNumber = 1;
+        }
+    }
+}
