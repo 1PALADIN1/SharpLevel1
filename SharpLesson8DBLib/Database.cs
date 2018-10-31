de0f0d359f3a13717ba583e34c04ed000edc401c
1PALADIN1 <sonikx94@yandex.ru>
2018-10-31 22:23:27 +0300

    Добавил библиотеку для базы данных

---
+++ SharpLesson8DBLib/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace SharpLesson8DBLib
+{
+    // Класс для хранения списка вопросов. А так же для сериализации в XML и десериализации из XML
+    class Database {
+        string fileName;
+        List<Question> list;
+
+        public List<Question> List {
+            get { return list; }
+            set { list = value; }
+        }
+
+        public string FileName {
+            set { fileName = value; }
+            get { return fileName; }
+        }
+
+        public Database(string fileName) {
+            this.fileName = fileName;
+            list = new List<Question>();
+        }
+
+        public void Save() {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+
+        public void Load() {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            list = (List<Question>)xmlFormat.Deserialize(fStream);
+            fStream.Close();
+        }
+
+        public int Count {
+            get { return list.Count; }
+        }
+    }
+}
---
+++ SharpLesson8DBLib/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLesson8DBLib {
+    // Класс для вопроса    
+    [Serializable]
+    public class Question {
+        string text;       // Текст вопроса
+        bool trueFalse;// Правда или нет
+
+        public string Text {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public bool TrueFalse {
+            get { return trueFalse; }
+            set { trueFalse = value; }
+
+        }
+
+        public Question() { }
+
+        public Question(string text, bool trueFalse) {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+    }
+}
