c1ba59ee65d518e3fae7c4589ec378066adb668b
1PALADIN1 <sonikx94@yandex.ru>
2018-10-31 09:43:51 +0300

    init database projetc

---
+++ SharpLesson8DB/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Малиновский Руслан */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpLesson8DB {
+    static class Program {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
